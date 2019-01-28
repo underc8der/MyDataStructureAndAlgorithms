f926c0c99432fdfaefeabb725680fbd4757d1ae1
David N <davidn.g0015@gmail.com>
2019-01-28 13:02:41 -0500

    Mario's algorithm

--- main/com/samples/sort/MergeSort.java
+++ main/com/samples/sort/MergeSort.java
@@ -1,63 +1,33 @@
 package com.samples.sort;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class MergeSort {
 
     public static void sort(int[] array) {
-	divide(array, 0, array.length - 1);
+	divide(array);
 	Arrays.stream(array).forEach(System.out::print);
     }
 
-    protected static void divide(int[] array, int min, int max) {
-	if (min < max) {
-	    int middle = min + (max - min) / 2;
-	    divide(array, min, middle);
-	    divide(array, middle + 1, max);
-	    merge(array, min, middle, max);
-	}
-    }
-    
-    protected static void divide2(int[] array, int min, int max) {
-	if (min < max) {
-	    int mid = min + (max - min) / 2;
+    protected static void divide(int[] array) {
+	int n = array.length;
+	if (1 < array.length) {
+	    int mid = n / 2;
 	    int[] la = new int[mid];
-	    int[] ra = new int[max - mid];
+	    int[] ra = new int[n - mid];
 	    for (int i = 0; i < mid; i++) {
-		la[i] = array[mid - min];
+		la[i] = array[i];
 	    }
-	    for (int i = 0; i < mid; i++) {
-		ra[i] = array[max - mid]; 
+	    for (int i = 0; i < (n - mid); i++) {
+		ra[i] = array[i + mid]; 
 	    }
-	    divide(array, min, mid);
-	    divide(array, mid + 1, max);
-	    merge(array, la, ra, min, max);
+	    divide(la);
+	    divide(ra);
+	    merge(array, la, ra, 0, array.length);
 	}
     }
     
-    private static void merge(int[] array, int min, int mid, int max) {
-	int[] temp = new int[array.length];
-	for (int i = min; i <= max; i++) {
-	    temp[i] = array[i];
-	}
-	int i = min;
-	int j = mid;
-	for (int k = min; k < mid; k++) {
-	    if (temp[i] < temp[j]) {
-//		array[k] = temp[i];
-//		swap(array, temp, k, i);
-//		i++;
-	    } else {
-//		array[k] = temp[j];
-		swap(array, temp, k, j);
-		j++;
-	    }
-	}
-    }
-    
-    protected static void merge(int[]dest, int[]la, int[]ra, int low, int high) {
+    private static void merge(int[]dest, int[]la, int[]ra, int low, int high) {
 	int li = 0;
 	int ri = 0;
 	for (int i = low; i < high; i++) {
@@ -68,17 +38,5 @@ public class MergeSort {
 	    }
 	}
 	
-    }
-    
-    /**
-     * moves the current position to the next one and vice-versa
-     * 
-     * @param array
-     * @param current
-     * @param next
-     */
-    private static void swap(int[] array, int[] temp, int current, int next) {
-	array[next] = temp[current];
-	array[current] = temp[next];
     }
 }
