9a68c7c04b8630f37a84a156b06113741950d904
David N <davidn.g0015@gmail.com>
2019-02-01 11:15:13 -0500

    HashTable without collision handling

--- main/com/samples/exercise/StringManipulationHashed.java
+++ main/com/samples/exercise/StringManipulationHashed.java
@@ -1,37 +1,29 @@
 package com.samples.exercise;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class StringManipulationHashed {
     
     public static boolean find(String source, String seek) {
 	int seekLength = seek.length();
 	int sourceLength = (source.length() - seekLength);
-	if (seekLength > source.length()) {
-	    return false;
-	}
-	Map<String, Integer> hashedValues = new HashMap<>();
-	for(int i = 0; i <= sourceLength; i++) {
-	    hashedValues.put(source.substring(i,  i + seekLength), 1);
+	
+	int sizeHash = source.length() - (seek.length() - 1) ;
+	int key = generateHash(seek, sizeHash);
+	
+	String[] tokens = new String[sizeHash];
+	for (int i = 0; i <= sourceLength; i++) {
+	    String subStr = source.substring(i,  i + seekLength);
+	    int hash = generateHash(subStr, sizeHash);
+	    tokens[hash] = subStr;
 	}
 	
-	return hashedValues.containsKey(seek);
+	return tokens[key] != null && seek.equals(tokens[key]);
     }
     
-    public static void main(String[] args) {
-	char a = 'z';
-	int b = a >> 1;
-	System.out.println(1 << a - 'z');
-	System.out.println(b);
-    }
-    
-    static long hash(String subStr)
-    {
-        long hash = 5381;
-        for (char c: subStr.toCharArray())
-            hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
-        return hash;
+    private static int generateHash(String subStr, int sizeHash) {
+	int sum = 0;
+	for (int i = 0; i < subStr.length(); i++) {
+	    sum += ((int)subStr.charAt(i)) * (26 ^ (subStr.length() - (i + 1)));
+	}
+	return sum % sizeHash;
     }
 }
--- test/com/samples/exercise/StringManipulationHashedTest.java
+++ test/com/samples/exercise/StringManipulationHashedTest.java
@@ -3,6 +3,8 @@ package com.samples.exercise;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.junit.Test;
 
 public class StringManipulationHashedTest {
@@ -17,9 +19,23 @@ public class StringManipulationHashedTest {
     
     @Test
     public void find_NonExistingWord_succesful() {
-	String source = "abcdefghlmnopqrstuijk";
-	String seek = "zqk";
+	String source = "abcd";
+	String seek = "zqa";
 	boolean exist = StringManipulationHashed.find(source, seek);
 	assertFalse(exist);
+    }
+    
+    @Test
+    public void find_RandomValues_Succesful() {
+	StringBuilder source = new StringBuilder(11);
+	for (int i = 0; i < 11; i++) {
+	    char c = (char)ThreadLocalRandom.current().nextInt(97, 122);
+	    source.append(c);
+	}
+	int init = ThreadLocalRandom.current().nextInt(8);
+	String seek = source.toString().substring(init, init + 2);
+	System.out.println(seek + " :: " + source.toString());
+	boolean exist = StringManipulationHashed.find(source.toString(), seek);
+	assertTrue(exist);
     }
 }
